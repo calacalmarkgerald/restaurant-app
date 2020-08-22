3a9ea6d5ee5e67496f71caacfde8f718b041cede
Mark Gerald Calacal <mcalacal@stratpoint.com>
2020-08-23 02:54:11 +0800

    converted the project to typescript and created the get-restaurant lambda

---
+++ functions/get-restaurants.ts
@@ -0,0 +1,53 @@
+import { DynamoDB } from 'aws-sdk';
+import { DocumentClient, ItemList, ScanInput } from 'aws-sdk/clients/dynamodb';
+import { APIGatewayEvent, Context, APIGatewayProxyResult } from 'aws-lambda';
+
+const tableName: string = process.env.restaurants_table!;
+const client: DocumentClient = new DynamoDB.DocumentClient();
+const defaultResults: number = parseInt(process.env.defaultResults!) || 8;
+
+/**
+ * Returns a list of restaurants
+ * @param {number} count
+ * @param {DocumentClient} client
+ * @param {string} tableName
+ */
+export const getRestaurants = async (count: number, client: DocumentClient, tableName: string): Promise<ItemList> => {
+  let items: ItemList = [];
+  const params: ScanInput = {
+    TableName: tableName,
+    Limit: count,
+  };
+
+  try {
+    const data = await client.scan(params).promise();
+    if (data.Items) {
+      items = data.Items;
+    }
+  } catch (error) {
+    console.error(error);
+    throw new Error('Unable to scan data');
+  }
+
+  return items;
+};
+
+/**
+ * Lambda function handler that takes a HTTP event from API GW
+ *
+ * @param {APIGatewayEvent} event
+ * @param {Context} context
+ * @returns {Promise<APIGatewayProxyResult>}
+ */
+export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
+  if (!tableName) {
+    throw new Error('Table name is undefined');
+  }
+
+  const restaurants: ItemList = await getRestaurants(defaultResults, client, tableName);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(restaurants),
+  };
+};
